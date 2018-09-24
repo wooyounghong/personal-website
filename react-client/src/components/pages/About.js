9a94da1e2abe681d81da1ff559a11517d85f5c0b
Wayne Hong <woo.young.hong@hotmail.com>
2018-09-24 17:37:12 +0900

    basic react-route set up with Homepage component and About component, and created Menu using SemanticsUI

---
+++ react-client/src/components/pages/About.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const About = () => <div>About Wayne...</div>;
+
+export default About;
---
+++ react-client/src/components/pages/NotFound.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const NotFound = () => <div>Route not found!</div>;
+
+export default NotFound;
