dd91c4fad86e2b1674e7cd72ab53d5130249e9a5
Wayne Hong <woo.young.hong@hotmail.com>
2018-09-25 15:24:12 +0900

    update

--- react-client/src/components/app.js
+++ react-client/src/components/app.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Link} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import { browserHistory } from 'react-router';
-import HomePage from './pages/homePage.js';
-import NavBar from './headerComponent/navBar.js';
-import Footer from './footerComponent/footer.js';
+import HomePage from './pages/HomePage';
+import About from './pages/About';
+import NavBar from './headerComponent/NavBar.js';
+import Footer from './footerComponent/Footer.js';
 
 class App extends Component {
   render() {
@@ -12,10 +13,12 @@ class App extends Component {
         <div>
           <NavBar />
           <Route name="home" exact path="/" component={HomePage} />
+          <Route name="projects" exact path="/projects" />
+          <Route name="about" exact path="/about" component={About} />
           <Footer />
         </div>
       </Router>
-    )
+    );
   }
 }
 
--- react-client/src/components/headerComponent/navBar.js
+++ react-client/src/components/headerComponent/navBar.js
@@ -1,15 +1,53 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { Menu } from 'semantic-ui-react';
+import { browserHistory } from 'react-router';
 
 class NavBar extends Component {
+  constructor() {
+    super();
+    this.state = { activeItem: 'home' };
+    this.handleClick = this.handleClick.bind(this);
+  }
+  handleClick(e, { name }) {
+    e.preventDefault();
+    this.setState(
+      {
+        activeItem: name,
+      },
+      () => {
+        console.log('Menu pressed', name, this.props);
+      }
+    );
+  }
   render() {
+    const { activeItem } = this.state;
     return (
-      <header>
-        <ul id="headerButtons">
-          <li className="navButton"><Link to="">Home</Link></li>
-        </ul>
-      </header>
-    )
+      <Menu secondary>
+        <Menu.Item
+          name="home"
+          as={Link}
+          to="/home"
+          active={activeItem === 'home'}
+        />
+
+        {/* <Menu.Item
+          name="home"
+          active={activeItem === 'home'}
+          onClick={this.handleClick}
+        /> */}
+        <Menu.Item
+          name="projects"
+          active={activeItem === 'projects'}
+          onClick={this.handleClick}
+        />
+        <Menu.Item
+          name="about"
+          active={activeItem === 'about'}
+          onClick={this.handleClick}
+        />
+      </Menu>
+    );
   }
 }
 
