1731f9e3d32a92a87ec741fab103af9f4ef50290
Wayne Hong <woo.young.hong@hotmail.com>
2018-09-30 21:27:33 +0900

    had to take out data folder

---
+++ data/data.js
@@ -0,0 +1,63 @@
+const portfolioData = {
+  portfolioData: [
+    {
+      title: 'Portfolio Website',
+      description:
+        'A web application which users to see and learn about Wayne and his projects',
+      icons: ['react', 'js square', 'node', 'aws'],
+      links: [
+        {
+          url: 'https://github.com/wooyounghong/personal-website',
+          icon: 'github',
+        },
+        {
+          url: 'localhost:8080',
+          icon: 'internet explorer',
+        },
+      ],
+    },
+    {
+      title: 'NowOrLater',
+      description:
+        'A react-native mobile application which allows users to send or receive messages based on time or geolocation',
+      icons: ['js square', 'react', 'npm', 'google'],
+      links: [
+        {
+          url: 'https://github.com/WAtCH-capstone/capstone',
+          icon: 'github',
+        },
+        {
+          url: '',
+          icon: 'internet explorer',
+        },
+      ],
+    },
+    {
+      title: 'Stocks-To-Go',
+      description:
+        'This app will help generate a financial stock graph using IEX API. User will see the rendered graph and pick the length of time frame',
+      icons: ['react', 'npm', 'google'],
+      links: [
+        {
+          url: 'https://github.com/wooyounghong/Stocks-To-Go',
+          icon: 'github',
+        },
+      ],
+    },
+    {
+      title: 'React Context API vs Redux',
+      description:
+        "Blog post that introduces React's updated Context API vs Redux",
+      icons: ['js square', 'react'],
+      links: [
+        {
+          url:
+            'https://medium.com/@hongwy1128/the-end-of-redux-beginning-of-context-api-ba8f09e2d7d9',
+          icon: 'medium',
+        },
+      ],
+    },
+  ],
+};
+
+export default portfolioData;
